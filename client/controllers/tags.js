347d258a872b9d9c8a65911f0774995730a0ec12
AdamHKaplan <AdamHKaplan@gmail.com>
2014-12-31 15:12:47 -0800

    tag marquee styling & js optimization

--- client/controllers/tags.js
+++ client/controllers/tags.js
@@ -1,7 +1,6 @@
 // tags.js
 angular.module('xyz')
   .controller('TagsCtrl', function($scope, $interval, Tags){
-    $scope.tags = ['abcd', 'efg']
     $scope.news = $scope.tags;
     $scope.conf = {
         news_length: false,
@@ -9,19 +8,21 @@ angular.module('xyz')
         news_margin: 20,
         news_move_flag: true
     };
-
+    $scope.$watch('tags', function(){
+      $scope.conf.news_pos = 200;
+    })
     $scope.init = function() {
         // $http.post('the_news_file.json', null).success(function(data) {
             // if (data && data.length > 0) {
                 $scope.news = $scope.tags;
-                $interval($scope.news_move, 50);
+                $interval($scope.news_move, 1);
             // }
         // });
     };
 
     $scope.get_news_right = function(idx) {
         var $right = $scope.conf.news_pos;
-        for (var ri=0; ri < idx; ri++) {
+        for (var ri=0; ri < 1; ri++) {
             if (document.getElementById('news_'+ri)) {
                 $right += $scope.conf.news_margin + angular.element(document.getElementById('news_'+ri))[0].offsetWidth;
             }
@@ -30,16 +31,14 @@ angular.module('xyz')
     };
 
     $scope.news_move = function() {
-      console.log('heeee');
         if ($scope.conf.news_move_flag) {
             $scope.conf.news_pos--;
-            console.log($scope.conf.news_pos);
-            if ( angular.element(document.getElementById('news_0'))[0].offsetLeft > angular.element(document.getElementById('news_strip'))[0].offsetWidth + $scope.conf.news_margin ) {
-                var first_new = $scope.news[0];
-                $scope.news.push(first_new);
-                $scope.news.shift();
-                $scope.conf.news_pos += angular.element(document.getElementById('news_0'))[0].offsetWidth + $scope.conf.news_margin;
-            }
+            // if ( angular.element(document.getElementById('news_0'))[0].offsetWidth > angular.element(document.getElementById('news_strip'))[0].offsetWidth + $scope.conf.news_margin ) {
+            //     var first_new = $scope.news[0];
+            //     $scope.news.push(first_new);
+            //     $scope.news.shift();
+            //     $scope.conf.news_pos += angular.element(document.getElementById('news_0'))[0].offsetWidth + $scope.conf.news_margin;
+            // }
         }
     };
   })
